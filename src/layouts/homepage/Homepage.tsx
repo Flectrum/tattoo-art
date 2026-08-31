1b957363368f9cd7079d71456e94af9d5c95af48
Aleksandr Berdnikov <93481178+Flectrum@users.noreply.github.com>
2026-08-31 23:10:14 +0300

    Add files via upload

--- src/layouts/homepage/Homepage.tsx
+++ src/layouts/homepage/Homepage.tsx
@@ -4,9 +4,9 @@ import { HeroBackground } from "./HeroBackground";
 export const Homepage = () => {
   return (
     <>
-      <div className="relative overflow-hidden">
+      <div className="relative w-full top-0 text-center align-center justify-center overflow-hidden">
         <HeroBackground />
-        <div className="absolute w-full top-20 text-center mt-20 align-center justify-center ">
+        <div className="absolute ">
           <Hero />
         </div>
       </div>
