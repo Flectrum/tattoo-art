91f916b11c4d2bf720854632868f02a171b6da95
Aleksandr Berdnikov <93481178+Flectrum@users.noreply.github.com>
2026-09-01 00:39:41 +0300

    Add files via upload

--- src/layouts/homepage/Homepage.tsx
+++ src/layouts/homepage/Homepage.tsx
@@ -4,7 +4,10 @@ import { HeroBackground } from "./HeroBackground";
 export const Homepage = () => {
   return (
     <>
-      <div className="relative isolate flex min-h-[600px] items-center justify-center overflow-hidden text-center">
+      <div
+        className="relative isolate flex min-h-[calc(100svh-4rem)] md:min-h-[calc(100dvh-4rem)]
+       items-center justify-center overflow-hidden text-center"
+      >
         <HeroBackground />
         <div className="relative z-10">
           <Hero />
