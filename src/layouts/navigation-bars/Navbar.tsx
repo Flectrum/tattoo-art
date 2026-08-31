ac1588726cf2df682105e1b23fb3a935a2558748
Aleksandr Berdnikov <93481178+Flectrum@users.noreply.github.com>
2026-09-01 01:00:06 +0300

    Add files via upload

--- src/layouts/navigation-bars/Navbar.tsx
+++ src/layouts/navigation-bars/Navbar.tsx
@@ -7,7 +7,7 @@ import { NavLink } from "react-router-dom";
 export const Navbar = () => {
   return (
     <header className="sticky top-0 z-40 backdrop-blur-lg border-b-2 border-b-red-900">
-      <nav className="flex h-18 items-center justify-between gap-10 px-5 md:px-0 md:container md:mx-auto lg:container lg:mx-auto">
+      <nav className="flex h-18 items-center justify-between gap-10 px-5 md:px-0 md:container md:mx-auto lg:container">
         <div className="flex" aria-label="Tattoo Art">
           <div className="flex items-center justify-between gap-3">
             <img src={logo} alt="Tattoo Art" width={40} height={40} />
