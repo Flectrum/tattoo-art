ab4e4a687e161536bfb72228a475c8929c69315f
Flectrum <aleksandr.runer@gmail.com>
2026-09-08 23:43:46 +0300

    booking page refactored and functionality added

--- src/layouts/booking/Booking.tsx
+++ src/layouts/booking/Booking.tsx
@@ -4,74 +4,113 @@ import { Instagram } from "../../assets/svg/Instagram";
 import { Email } from "../../assets/svg/Email";
 import { WhatsApp } from "../../assets/svg/WhatsApp";
 
+interface ContactMethod {
+  name: string;
+  svg: React.ReactNode;
+  label: string;
+  placeholder: string;
+  contact: string;
+}
+
 interface FormData {
   name: string;
   phoneNumber: string;
-  communicationType: string;
+  contactMethod: ContactMethod | null;
+  contact: string;
 }
 
-interface ConnectionType {
-  name: string;
-  svg: React.ReactNode;
+interface Errors {
+  name?: string;
+  phoneNumber?: string;
+  contactMethod?: string;
+  contact?: string;
 }
 
-type FormErrors = Partial<Record<keyof FormData, string>>;
+const contactMethods: ContactMethod[] = [
+  {
+    name: "Telegram",
+    svg: <Telegram />,
+    label: "Your Telegram username or phone number",
+    placeholder: "@username or +372 XXXX XXXX",
+    contact: "",
+  },
+  {
+    name: "Instagram",
+    svg: <Instagram />,
+    label: "Your Instagram handle",
+    placeholder: "@yourhandle",
+    contact: "",
+  },
+  {
+    name: "WhatsApp",
+    svg: <WhatsApp />,
+    label: "Your WhatsApp number",
+    placeholder: "+372 XXXX XXXX",
+    contact: "",
+  },
+  {
+    name: "Email",
+    svg: <Email />,
+    label: "Your email address",
+    placeholder: "your@email.com",
+    contact: "",
+  },
+];
 
 export const Booking = () => {
-  const [warnings, setWarnings] = useState<FormErrors>({});
-  const [formStep] = useState(1);
+  const [step, setStep] = useState(1);
+
   const [formData, setFormData] = useState<FormData>({
     name: "",
     phoneNumber: "",
-    communicationType: "",
+    contactMethod: null,
+    contact: "",
   });
 
-  const connectionTypes: ConnectionType[] = [
-    { name: "Telegram", svg: <Telegram /> },
-    { name: "Instagram", svg: <Instagram /> },
-    { name: "WhatsApp", svg: <WhatsApp /> },
-    { name: "Email", svg: <Email /> },
-  ];
+  const [formErrors, setFormErrors] = useState<Errors>({});
 
-  const hadleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const name = e.target.name as keyof FormData;
-    const value = e.target.value;
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
 
     setFormData((prev) => ({ ...prev, [name]: value }));
 
-    if (warnings[name]) {
-      setWarnings((prev) => ({ ...prev, [name]: undefined }));
-    }
+    setFormErrors((prev) => ({
+      ...prev,
+      [name]: undefined,
+    }));
   };
 
-  const handleClick = (type: string) => {
-    setFormData((prev) => ({ ...prev, communicationType: type }));
-
-    if (warnings.communicationType) {
-      setWarnings((prev) => ({ ...prev, connectionType: undefined }));
-    }
+  const handleContactMethod = (type: ContactMethod) => {
+    setFormData((prev) => ({ ...prev, contactMethod: type, contact: "" }));
+    setFormErrors((prev) => ({
+      ...prev,
+      contactMethod: undefined,
+      contact: "",
+    }));
   };
 
-  const handleNextButton = () => {
-    const newWarning: FormErrors = {
-      name: "",
-      phoneNumber: "",
-      communicationType: "",
-    };
+  const handleNext = () => {
+    const errors: Errors = {};
 
     if (!formData.name.trim()) {
-      newWarning.name = "This field is required";
+      errors.name = "This field is required";
     }
     if (!formData.phoneNumber.trim()) {
-      newWarning.phoneNumber = "This field is required";
+      errors.phoneNumber = "This field is required";
     }
-    if (!formData.communicationType.trim()) {
-      newWarning.communicationType = "This field is required";
+    if (!formData.contactMethod) {
+      errors.contactMethod = "This field is required";
     }
 
-    setWarnings(newWarning);
+    if (!formData.contact?.trim()) {
+      errors.contact = "This field is required";
+    }
 
-    console.log(formData.communicationType);
+    setFormErrors(errors);
+
+    if (Object.keys(errors).length === 0) {
+      setStep(step + 1);
+    }
   };
 
   return (
@@ -88,7 +127,7 @@ export const Booking = () => {
           </a>
         </div>
         <form method="POST" className="flex flex-col mt-10">
-          {formStep === 1 && (
+          {step === 1 && (
             <div className="flex flex-col mt-10">
               <div className="flex  font-bold text-2xl">Your Details</div>
               <label className="mt-5">
@@ -98,12 +137,12 @@ export const Booking = () => {
                 className=" mt-2 rounded-lg px-2 md:px-5 py-2 border-1 border-gray-600/80 "
                 type="text"
                 name="name"
-                onChange={hadleChange}
+                onChange={handleChange}
                 value={formData.name}
                 placeholder="Your name"
               />
-              {warnings.name && (
-                <p className="text-red-500 text-sm">{warnings.name}</p>
+              {formErrors.name && (
+                <p className="text-red-500 text-sm">{formErrors.name}</p>
               )}
               <label className="mt-2">
                 Phone number <span className="text-red-800">*</span>
@@ -112,11 +151,11 @@ export const Booking = () => {
                 className=" mt-2 rounded-lg px-2 md:px-5 py-2 border-1 border-gray-600/80"
                 type="text"
                 name="phoneNumber"
-                onChange={hadleChange}
+                onChange={handleChange}
                 value={formData.phoneNumber}
                 placeholder="+372 XXXX XXXX"
               />
-              {warnings.phoneNumber && (
+              {formErrors.phoneNumber && (
                 <p className="text-red-500 text-sm">This field is required</p>
               )}
               <label className="mt-2">
@@ -124,15 +163,15 @@ export const Booking = () => {
                 <span className="text-red-800">*</span>
               </label>
               <ul className="flex gap-3 mt-3">
-                {connectionTypes.map((item, index) => (
-                  <li key={index} className="">
+                {contactMethods.map((item, index) => (
+                  <li key={index}>
                     <button
                       type="button"
-                      value={formData.communicationType}
-                      onClick={() => handleClick(item.name)}
-                      name="connectionType"
+                      value={formData.contactMethod?.name}
+                      onClick={() => handleContactMethod(item)}
+                      name="connectionType.name"
                       className={`relative flex items-center rounded-lg border-1  
-                ${formData.communicationType === item.name ? "text-red-500 border-red-500" : "text-[#737373] border-[#737373] hover:text-white"}`}
+                ${formData.contactMethod?.name === item.name ? "text-red-500 border-red-500" : "text-[#737373] border-[#737373] hover:text-white"}`}
                     >
                       <div className="mx-3 my-2 flex items-center gap-2">
                         <div className="w-5">{item.svg}</div>
@@ -142,22 +181,25 @@ export const Booking = () => {
                   </li>
                 ))}
               </ul>
-              {warnings.communicationType && (
+              {formErrors.contactMethod && (
                 <p className="text-red-500 text-sm">This field is required</p>
               )}
-              {formData.communicationType !== "" && (
+              {formData.contactMethod && (
                 <>
                   <label className="mt-2">
-                    Nickname <span className="text-red-800">*</span>
+                    {formData.contactMethod?.label}{" "}
+                    <span className="text-red-800">*</span>
                   </label>
                   <input
                     className=" mt-2 rounded-lg px-2 md:px-5 py-2 border-1 border-gray-600/80"
                     type="text"
                     required
-                    onChange={hadleChange}
-                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    value={formData.contact}
+                    name="contact"
+                    placeholder={formData.contactMethod?.placeholder}
                   />
-                  {warnings.communicationType && (
+                  {formErrors.contact && (
                     <p className="text-red-500 text-sm">
                       This field is required
                     </p>
@@ -168,14 +210,14 @@ export const Booking = () => {
                 <button
                   type="button"
                   className="mt-3 h-10 border-1 border-white bg-red-500 rounded-lg hover:bg-red-600"
-                  onClick={() => handleNextButton()}
+                  onClick={() => handleNext()}
                 >
                   <span className="mx-8 font-bold text-xl">Next </span>
                 </button>
               </div>
             </div>
           )}
-          {formStep === 2 && <>SECOND STEP</>}
+          {step === 2 && <>SECOND STEP</>}
         </form>
       </div>
     </>
