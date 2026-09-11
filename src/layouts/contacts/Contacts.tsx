51b87d61bf65adc65434e0f9cd0f5401ca9a8f69
Flectrum <aleksandr.runer@gmail.com>
2026-09-11 15:54:28 +0300

    contacts page updated

--- src/layouts/contacts/Contacts.tsx
+++ src/layouts/contacts/Contacts.tsx
@@ -12,7 +12,7 @@ export const Contacts = () => {
         <p className="mt-2 text-muted">Get in touch</p>
         <div className="mt-10 grid gap-12 lg:grid-cols-2">
           <div className="space-y-6 text-white">
-            <div className=" ml-3 flex gap-3 border border-[#484747] w-2/3 bg-[#141414] hover:bg-[#353434] rounded-md p-2">
+            <div className="flex gap-3 border border-[#484747] w-2/3 bg-[#141414] hover:bg-[#353434] rounded-md p-2">
               <Telegram className="w-6" />
               <div>
                 <p className="text-muted">Telegram</p>
