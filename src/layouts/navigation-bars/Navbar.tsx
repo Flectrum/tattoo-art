a2dcd296f81f3ae0611cabf34813f58820a87051
Flectrum <aleksandr.runer@gmail.com>
2026-09-12 19:23:22 +0300

    logo hover added

--- src/layouts/navigation-bars/Navbar.tsx
+++ src/layouts/navigation-bars/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "../../assets/svg/Logo";
 import logo from "./../../assets/logo.svg";
 import { NavbarDesktop } from "./components/NavbarDesktop";
 import { NavbarMobile } from "./components/NavbarMobile";
@@ -9,10 +10,10 @@ export const Navbar = () => {
     <header className="sticky top-0 z-40 backdrop-blur-lg border-b-2 border-b-red-900">
       <nav className="flex h-18 items-center justify-between gap-10 px-5 md:px-0 md:container md:mx-auto">
         <div className="flex" aria-label="Tattoo Art">
-          <div className="flex items-center justify-between gap-3">
-            <img src={logo} alt="Tattoo Art" width={40} height={40} />
+          <div className="flex items-center justify-between gap-3 transition-colors text-white hover:text-red-800">
+            <Logo className="w-10 h-10" />
             <NavLink
-              className="font-semibold text-nowrap uppercase tracking-widest text-white hover:text-red-800 transition-colors"
+              className="font-semibold text-nowrap uppercase tracking-widest"
               to="/"
             >
               Tattoo Art
