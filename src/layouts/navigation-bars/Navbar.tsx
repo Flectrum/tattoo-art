97df7c88faf2877958c446d6417d8a6868b88480
Flectrum <aleksandr.runer@gmail.com>
2026-09-16 00:00:09 +0300

    navbar fixes

--- src/layouts/navigation-bars/Navbar.tsx
+++ src/layouts/navigation-bars/Navbar.tsx
@@ -13,15 +13,16 @@ export const Navbar = () => {
     <header className="sticky top-0 z-40 backdrop-blur-lg border-b-2 border-b-red-900">
       <nav className="flex h-18 items-center justify-between gap-10 md:px-0 md:container md:mx-auto lg:px-20">
         <div className="flex" aria-label="Tattoo Art">
-          <div className="flex items-center justify-between gap-3 transition-colors text-white hover:text-red-800 px-5">
-            <Logo className="w-10 h-10" />
-            <NavLink
-              className="font-semibold text-nowrap uppercase tracking-widest"
-              to="/"
-            >
+          <NavLink
+            className="font-semibold text-nowrap uppercase tracking-widest"
+            to="/"
+          >
+            <div className="flex items-center justify-between gap-3 transition-colors text-white hover:text-red-800 px-5">
+              <Logo className="w-10 h-10" />
               Tattoo Art
-            </NavLink>
-          </div>
+            </div>
+          </NavLink>
+          ={" "}
         </div>
         <div className="flex items-center px-5">
           {/* Mobile */}
